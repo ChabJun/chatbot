ba8c422ed00d841fffa214804ce0347cb4b13c3a
ChabJun <35719362+ChabJun@users.noreply.github.com>
2018-01-25 16:57:34 +0900

    HTTP Server

--- server.py
+++ server.py
@@ -1,15 +1,5 @@
 import http.server
 
-HTTPHandler = http.server.SimpleHTTPRequestHandler
+httpd = http.server.HTTPServer(('0.0.0.0', 8080), http.server.SimpleHTTPRequestHandler)
 
-HTTPHandler.extension_map.update ({
-
-    '.m3u8' : 'application/x-xmpegURL',
-
-    '.ts' : 'video/MP2T'
-
-})
-
-httpd = http.server.HTTPServer(('0.0.0.0', 8080), HTTPHandler)
-
-httpd.server_forever()
+httpd.serve_forever()
